92699b889f69d434ee7d018abf8994c75a106b89
nhandinhto <todinhnhan1997@gmail.com>
2022-06-29 10:19:27 +0700

    Lession : Build Product Component

---
+++ src/APM/src/app/products/product-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "pm-products",
+    templateUrl:`./products-list.component.html`,
+})
+
+export class ProductListComponent{
+    title : string = "Products List Components"
+}
